79db90d1c23f571bc0c85bdc39a7325913401ce7
Kevin Peng <kpeng66@gmail.com>
2023-07-10 17:17:07 -0700

    play/pause functionality implemented

--- frontend/src/components/MusicPlayer.js
+++ frontend/src/components/MusicPlayer.js
@@ -1,39 +1,77 @@
-import React, { useState, useEffect } from 'react';
-import { Grid, Typography, Card, IconButton, LinearProgress } from '@mui/material';
+import React, { useState, useEffect } from "react";
+import {
+  Grid,
+  Typography,
+  Card,
+  IconButton,
+  LinearProgress,
+} from "@mui/material";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import PauseIcon from "@mui/icons-material/Pause";
 import SkipNextIcon from "@mui/icons-material/SkipNext";
 
 const MusicPlayer = ({
-    time, duration, image_url, title, artist, is_playing
+  time,
+  duration,
+  image_url,
+  title,
+  artist,
+  is_playing,
 }) => {
-    const songProgress = (time / duration) * 100;
-    return (
-        <Card>
-            <Grid container alignItems="center">
-                <Grid item align='center' xs={4}>
-                    <img src={image_url} height='100%' width='100%'/>
-                </Grid>
-                <Grid item align='center' xs={8}>
-                    <Typography component="h5" variant="h5">
-                        {title}
-                    </Typography>
-                    <Typography color="textSecondary" variant="subtitle1">
-                        {artist}
-                    </Typography>
-                    <div>
-                        <IconButton>
-                            {is_playing ? <PauseIcon/> : <PlayArrowIcon/> }
-                        </IconButton>
-                        <IconButton>
-                            <SkipNextIcon/>
-                        </IconButton>
-                    </div>
-                </Grid>
-            </Grid>
-            <LinearProgress variant="determinate" value={songProgress}/>
-        </Card>
-    );
-}
+  const [isPlaying, setIsPlaying] = useState(is_playing);
 
-export default MusicPlayer
+  useEffect(() => {
+    setIsPlaying(is_playing);
+  }, [is_playing]);
+
+  const pauseSong = () => {
+    const requestOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+    };
+    fetch("/spotify/pause", requestOptions).then(() => {
+      setIsPlaying(false);
+    });
+  };
+
+  const playSong = () => {
+    const requestOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+    };
+    fetch("/spotify/play", requestOptions).then(() => {
+      setIsPlaying(true);
+    });
+  };
+
+  const songProgress = (time / duration) * 100;
+
+  return (
+    <Card>
+      <Grid container alignItems="center">
+        <Grid item align="center" xs={4}>
+          <img src={image_url} height="100%" width="100%" alt="Album Cover" />
+        </Grid>
+        <Grid item align="center" xs={8}>
+          <Typography component="h5" variant="h5">
+            {title}
+          </Typography>
+          <Typography color="textSecondary" variant="subtitle1">
+            {artist}
+          </Typography>
+          <div>
+            <IconButton onClick={isPlaying ? pauseSong: playSong}>
+              {isPlaying ? <PauseIcon /> : <PlayArrowIcon />}
+            </IconButton>
+            <IconButton>
+              <SkipNextIcon />
+            </IconButton>
+          </div>
+        </Grid>
+      </Grid>
+      <LinearProgress variant="determinate" value={songProgress} />
+    </Card>
+  );
+};
+
+export default MusicPlayer;
