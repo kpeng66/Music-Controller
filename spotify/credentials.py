c322057f917f040eb779ee1683c941f17e781d79
Kevin Peng <kpeng66@gmail.com>
2023-08-23 15:21:17 -0700

    changed spotify redirect uri

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,3 +1,3 @@
 CLIENT_ID = "1ceeff67772749db94214eb0e77fad77"
 CLIENT_SECRET = "9d88dbb9ad254c53a25f96200104ddf8"
-REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
+REDIRECT_URI = "https://houseparty-42f08c977e5f.herokuapp.com/spotify/redirect"
